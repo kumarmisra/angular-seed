b08cc04cdb29c4f0736d626baec46e5fa76802a7
kumarmisra <misra.kumar@gmail.com>
2014-12-28 15:30:25 -0700

    Firstiteration
    First iteration of changes

--- app/app.js
+++ app/app.js
@@ -1,12 +1,96 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var app=angular.module('MyUxApp', [
+  'myApp.version',
+    'ngRoute'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/members', {
+    templateUrl: 'view1/view1.html',
+    controller: 'membersController',
+      resolve: {
+		memberList: function(aquentuxsocietyAPIfactory) {
+			return aquentuxsocietyAPIfactory.getMembers();
+		}
+      }
+  })
+  .otherwise({redirectTo: '/members'});
+}])
+
+.controller('membersController',['$scope', 'memberList', function($scope, memberList, aquentuxsocietyAPIfactory) {
+    $scope.nameFilter = null;
+    $scope.memberList = memberList.data;
+    $scope.detail = [];
+    $scope.pageSize = 10;
+    $scope.currentPage = 0;
+    
+      $scope.numberOfPages=function(){
+        return Math.ceil($scope.memberList.length/$scope.pageSize);                
+    }
+    
+    $scope.searchFilter = function (member) {
+    var keyword = new RegExp($scope.nameFilter, 'i');
+    return !$scope.nameFilter || keyword.test(member.firstName);
+};
+    
+
+    $scope.getDetail = function(index){
+      $scope.detail = $scope.memberList[index -1];
+        //aquentuxsocietyAPIfactory.getDetail(index).success(function (response) {
+        //Dig into the responde to get the relevant data
+        //$scope.detail = response;
+   // });
+  };
+    
+  }])
+
+.factory('aquentuxsocietyAPIfactory', function($http) {
+
+    var urlBase = 'http://private-anon-0e04a959d-aquentuxsociety.apiary-mock.com/members';
+    var aquentuxsocietyAPIservice = {
+        
+    getMembers : function() {
+      var promise = $http({
+        method: 'GET', 
+        url: urlBase
+      }).success(function(data, status, headers, config) {
+				return data;
+			});
+    return promise;
+    },
+    getDetail : function(id) {
+			var promise = $http({ method: 'GET', url: urlBase + '/' +id }).success(function(data, status, headers, config) {
+				return data;
+			});
+			return promise;
+		}
+         
+}
+    return aquentuxsocietyAPIservice;
+  });
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
+
+app.run(['$rootScope', function($root) {
+
+  $root.$on('$routeChangeStart', function(e, curr, prev) { 
+    if (curr.$$route && curr.$$route.resolve) {
+      // Show a loading message until promises are not resolved
+      $root.loadingView = true;
+    }
+  });
+
+  $root.$on('$routeChangeSuccess', function(e, curr, prev) { 
+    // Hide loading message
+    $root.loadingView = false;
+  });
+
 }]);
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -1,16 +1,79 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+// Declare app level module which depends on views, and components
+var app=angular.module('MyUxApp', [
+  'myApp.version',
+    'ngRoute'
+])
 
-  beforeEach(module('myApp.view1'));
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/members', {
+    templateUrl: 'view1/view1.html',
+    controller: 'membersController'
+  })
+  .otherwise({redirectTo: '/members'});
+}])
 
-  describe('view1 controller', function(){
+.controller('membersController', function($scope, aquentuxsocietyAPIfactory) {
+    $scope.nameFilter = null;
+    $scope.memberList = [];
+    $scope.detail = [];
+    $scope.pageSize = 10;
+    $scope.currentPage = 0;
+    
+      $scope.numberOfPages=function(){
+        return Math.ceil($scope.memberList.length/$scope.pageSize);                
+    }
+    
+    $scope.searchFilter = function (member) {
+    var keyword = new RegExp($scope.nameFilter, 'i');
+    return !$scope.nameFilter || keyword.test(member.firstName);
+};
+    
+    
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
-    }));
+    aquentuxsocietyAPIfactory.getMembers().success(function (response) {
+        //Dig into the responde to get the relevant data
+        $scope.memberList = response;
+    });
+    
+    $scope.getDetail = function(index){
+      $scope.detail = $scope.memberList[index -1];
+        //aquentuxsocietyAPIfactory.getDetail(index).success(function (response) {
+        //Dig into the responde to get the relevant data
+        //$scope.detail = response;
+   // });
+  };
+    
+  })
 
+.factory('aquentuxsocietyAPIfactory', function($http) {
+
+    var urlBase = 'http://private-anon-0e04a959d-aquentuxsociety.apiary-mock.com/members';
+    var aquentuxsocietyAPIservice = {};
+    
+
+    aquentuxsocietyAPIservice.getMembers = function() {
+      return $http({
+        method: 'GET', 
+        url: urlBase
+      });
+    }
+    
+    aquentuxsocietyAPIservice.getDetail = function(id) {
+      return $http({
+        method: 'GET', 
+        url: urlBase + '/' +id
+      });
+    }
+
+    return aquentuxsocietyAPIservice;
   });
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
 });
